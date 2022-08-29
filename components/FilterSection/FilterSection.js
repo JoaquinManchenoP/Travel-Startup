09f9146a9c909ffd9a50511c03394a26f3c4ce82
JoaquinManchenoP <manchenojj@g.cofc.edu>
2022-08-29 12:52:31 -0400

    ui filters

--- components/FilterSection/FilterSection.js
+++ components/FilterSection/FilterSection.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 export default function FilterSection() {
     return (
-        <div className='h-36 w-full bg-green-400'>this is the filter section</div>
+        <div className='filter__container h-24 w-full flex items-center justify-center text-sm'>
+            <div className="filter__section h-12 w-11/12 flex items-center space-x-4 font-roboto font-bold">
+                <motion.div
+                    className="north__america__filter h-10 w-28 bg-slate-200 flex items-center justify-center rounded-md shadow-md cursor-pointer"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.99 }}
+                >
+                    North America
+                </motion.div>
+                <motion.div
+                    className="south__america__filter h-10 w-28 bg-slate-200 flex items-center justify-center rounded-md shadow-md cursor-pointer"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.99 }}
+                >
+                    South America
+                </motion.div>
+                <motion.div
+                    className="north__america__filter h-10 w-28 bg-slate-200 flex items-center justify-center rounded-md shadow-md cursor-pointer"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.99 }}
+                >
+                    Nort America
+                </motion.div>
+                <motion.div
+                    className="south__america__filter h-10 w-28 bg-slate-200 flex items-center justify-center rounded-md shadow-md cursor-pointer"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.99 }}
+                >
+                    Europe
+                </motion.div>
+            </div>
+        </div>
     )
 }
